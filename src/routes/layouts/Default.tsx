46a44d79bf7ca510595fc73d8d81f24a4099a4ac
SISUKYEONG <tltnrud@naver.com>
2025-05-16 16:11:23 +0900

    A2 삭제제

--- src/routes/layouts/Default.tsx
+++ src/routes/layouts/Default.tsx
@@ -1,18 +1,21 @@
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import { useOutlet } from 'react-router'
+import { useOutlet, useLocation } from 'react-router'
 import { motion, AnimatePresence } from 'framer-motion'
 
 export default function Default() {
   const outlet = useOutlet()
+  const location = useLocation()
   return (
     <>
       <Header />
       <AnimatePresence>
         <motion.div
+          key={location.pathname}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}>
+          exit={{ opacity: 0, position: 'absolute' }}
+          transition={{ duration: 0.3 }}>
           {outlet}
         </motion.div>
       </AnimatePresence>
